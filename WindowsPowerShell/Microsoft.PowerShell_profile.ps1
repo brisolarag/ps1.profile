0c9f8c5d5d3ec13e3db7626ee98d91a9aace7e95
Gabriel Ferreira Brisolara <dev.brisolara@gmail.com>
2024-08-28 10:11:31 -0300

    Update Microsoft.PowerShell_profile.ps1

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -45,12 +45,8 @@ function pkill($name) {
     Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
 }
 
-function nf { param($name) New-Item -ItemType "file" -Path . -Name $name }
-
-function docs { Set-Location -Path $HOME\Documents }
 function downloads { Set-Location -Path $HOME\Downloads }
-function pp { Set-Location -Path $HOME\Projects }
-function root { Set-Location -Path $HOME }
+function dtop { Set-Location -Path $HOME\Desktop }
 
 function gs { git status }
 
@@ -85,11 +81,6 @@ function Show-Help {
 PowerShell Profile Help
 =======================
 
-Update-Profile - Checks for profile updates from a remote repository and updates if necessary.
-
-Update-PowerShell - Checks for the latest PowerShell release and updates if a new version is available.
-
-Edit-Profile - Opens the current user's profile for editing using the configured editor.
 
 touch <file> - Creates a new empty file.
 
@@ -101,67 +92,19 @@ winutil - Runs the WinUtil script from Chris Titus Tech.
 
 uptime - Displays the system uptime.
 
-reload-profile - Reloads the current user's PowerShell profile.
-
 unzip <file> - Extracts a zip file to the current directory.
 
-hb <file> - Uploads the specified file's content to a hastebin-like service and returns the URL.
-
-grep <regex> [dir] - Searches for a regex pattern in files within the specified directory or from the pipeline input.
-
-df - Displays information about volumes.
-
-sed <file> <find> <replace> - Replaces text in a file.
-
-which <name> - Shows the path of the command.
-
-export <name> <value> - Sets an environment variable.
-
-pkill <name> - Kills processes by name.
-
-pgrep <name> - Lists processes by name.
-
-head <path> [n] - Displays the first n lines of a file (default 10).
-
-tail <path> [n] - Displays the last n lines of a file (default 10).
-
-nf <name> - Creates a new file with the specified name.
-
-mkcd <dir> - Creates and changes to a new directory.
-
-docs - Changes the current directory to the user's Documents folder.
+downloads - Changes the current directory to the user's Downloads folder.
 
 dtop - Changes the current directory to the user's Desktop folder.
-
-ep - Opens the profile for editing.
-
-k9 <name> - Kills a process by name.
-
-la - Lists all files in the current directory with detailed formatting.
-
-ll - Lists all files, including hidden, in the current directory with detailed formatting.
 
 gs - Shortcut for 'git status'.
 
 ga - Shortcut for 'git add .'.
 
-gc <message> - Shortcut for 'git commit -m'.
-
-gp - Shortcut for 'git push'.
-
-g - Changes to the GitHub directory.
-
-gcom <message> - Adds all changes and commits with the specified message.
-
-lazyg <message> - Adds all changes, commits with the specified message, and pushes to the remote repository.
-
 sysinfo - Displays detailed system information.
 
 flushdns - Clears the DNS cache.
-
-cpy <text> - Copies the specified text to the clipboard.
-
-pst - Retrieves text from the clipboard.
 
 Use 'Show-Help' to display this help message.
 "@
